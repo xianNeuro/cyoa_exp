bacc90fef77f280401c8bcf92682ba3a6aad3178
xianNeuro <43763564+xianNeuro@users.noreply.github.com>
2022-11-13 01:03:03 -0500

    Update cyoa_ctrl.js

--- monthiversary_free/cyoa_ctrl.js
+++ monthiversary_free/cyoa_ctrl.js
@@ -10000,26 +10000,25 @@ function time_record(event_click){
 }
 
 function toggleFullScreen() {
-  cyoa_spr();
-  // $('#consent').show();
-  // if ((document.fullScreenElement && document.fullScreenElement !== null) ||
-  //  (!document.mozFullScreen && !document.webkitIsFullScreen)) {
-  //   if (document.documentElement.requestFullScreen) {
-  //     document.documentElement.requestFullScreen();
-  //   } else if (document.documentElement.mozRequestFullScreen) {
-  //     document.documentElement.mozRequestFullScreen();
-  //   } else if (document.documentElement.webkitRequestFullScreen) {
-  //     document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
-  //   }
-  // } else {
-  //   if (document.cancelFullScreen) {
-  //     document.cancelFullScreen();
-  //   } else if (document.mozCancelFullScreen) {
-  //     document.mozCancelFullScreen();
-  //   } else if (document.webkitCancelFullScreen) {
-  //     document.webkitCancelFullScreen();
-  //   }
-  // }
+  $('#consent').show();
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||
+   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (document.documentElement.requestFullScreen) {
+      document.documentElement.requestFullScreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullScreen) {
+      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.cancelFullScreen) {
+      document.cancelFullScreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    }
+  }
   $('#askFull').hide();
 }
 
