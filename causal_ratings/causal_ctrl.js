11f089b9d796073f6b8fc174aaff6dbe37eb391c
xianNeuro <43763564+xianNeuro@users.noreply.github.com>
2023-07-01 23:11:40 +0800

    Create causal_ctrl.js

---
+++ causal_ratings/causal_ctrl.js
@@ -0,0 +1,60 @@
+
+//GLOBAL VARIABLES
+var mycounterbalance = Math.floor(Math.random() * 16) + 1; //have 16 subjects' story version left to collect causal rating on 
+
+// set route_vector and choice_vector variables
+if (mycounterbalance == 1) {
+  var route_subject= "sub100_4100"
+  var link = 'https://docs.google.com/document/d/1GAZ5XNV0tbgvGjxOTzW6SlQsxbf9TIb13a1PjKOfqhs/edit?usp=share_link' 
+} else if (mycounterbalance == 2) {
+  var route_subject= "sub10_4010"
+  var link = 'https://docs.google.com/document/d/10ivkt6JPBwBxb70al5rXhulj7Dc0O9Wz7sbn9AE8d1c/edit?usp=share_link' 
+} else if (mycounterbalance == 3) {
+  var route_subject= "sub14_4014"
+  var link = 'https://docs.google.com/document/d/1g6VyUl2oWBltvUEehEepmSDi7a3oda4gQ5LXaR6C1wk/edit?usp=share_link' 
+} else if (mycounterbalance == 4) {
+  var route_subject= "sub19_4019"
+  var link = 'https://docs.google.com/document/d/1HqyGbe-NDfYH0XymAtVF5BEpBMh5-NlSxm031DuFSZM/edit?usp=share_link' 
+} else if (mycounterbalance == 5) {
+  var route_subject= "sub23_4023"
+  var link = 'https://docs.google.com/document/d/132IO8cZCubVeVjTzIV3ZiZ99ZLRH3b8j87Vg4hpI0bQ/edit?usp=share_link' 
+} else if (mycounterbalance == 6) {
+  var route_subject= "sub25_4025"
+  var link = 'https://docs.google.com/document/d/18sMx5xeHlH30IUB9Az8tXGT71ZB7BGeJXtupGKcxDco/edit?usp=share_link' 
+} else if (mycounterbalance == 7) {
+  var route_subject= "sub37_4037"
+  var link = 'https://docs.google.com/document/d/1Gq6OfUh2Oe3ElKcYLFR0mDWX0k8lubp-L4zNwNbrmbs/edit?usp=share_link' 
+} else if (mycounterbalance == 8) {
+  var route_subject= "sub40_4040"
+  var link = 'https://docs.google.com/document/d/1zMXMafpR3IpUbvgu5GHxJqi47v-864v7TqVteQ7fahw/edit?usp=share_link' 
+} else if (mycounterbalance == 9) {
+  var route_subject= "sub50_4050"
+  var link = 'https://docs.google.com/document/d/1tz4jd37NOUfKn2z5zR8RcszXh3coilD2zEoAxltrQWk/edit?usp=share_link' 
+} else if (mycounterbalance == 10) {
+  var route_subject= "sub71_4071"
+  var link = 'https://docs.google.com/document/d/15fvCF_N1OwtBT0TSNzIYAVjGYcp6RB-mnya5lrWHj6A/edit?usp=share_link' 
+} else if (mycounterbalance == 11) {
+  var route_subject= "sub79_4079"
+  var link = 'https://docs.google.com/document/d/1tuyhkaS2ULOzANx_1wfdS_aox0QqQGZLprOF_f7bYrU/edit?usp=share_link' 
+} else if (mycounterbalance == 12) {
+  var route_subject= "sub80_4080"
+  var link = 'https://docs.google.com/document/d/19mjWm3a7ElgvOIA0ZRFcxpDVX5-Tacp-lWdaGIJBfFI/edit?usp=share_link' 
+} else if (mycounterbalance == 13) {
+  var route_subject= "sub81_4081"
+  var link = 'https://docs.google.com/document/d/1CNxwAo_74m4npVz_IpZUF-4OCVJVuhNCc7GTSUUoBRI/edit?usp=share_link' 
+} else if (mycounterbalance == 14) {
+  var route_subject= "sub90_4090"
+  var link = 'https://docs.google.com/document/d/1qgOGms75V0HIlS1OfHQAZhKBmjaPmoKJccImXvDxlBU/edit?usp=share_link' 
+} else if (mycounterbalance == 15) {
+  var route_subject= "sub94_4094"
+  var link = 'https://docs.google.com/document/d/1UJzcWIl8iMIMEObtCE-Z-3a_1chRam6mL3oIsthKCsc/edit?usp=share_link' 
+} else if (mycounterbalance == 16) {
+  var route_subject= "sub95_4095"
+  var link = 'https://docs.google.com/document/d/1mSLPQTHOcTXHb_tI77ZJ71hUJEfWenYi20TUCI12gs4/edit?usp=share_link' 
+};
+
+
+function assign_story(){
+  window.location=link
+}
+
